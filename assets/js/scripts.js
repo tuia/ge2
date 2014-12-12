b57fc0a907e570c74cb530e83226ae27e7eba3b9
Nathália <nathaliazxavier@gmail.com>
2014-12-12 18:06:10 -0200

    [mapa] subnava - scripts, estilos e transições

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,13 +6,32 @@ $(document).ready( function() {
 
 	//SUBNAV SEARCH BOX
 	$("#subnav .contextual-search").bind('focus', function() {
-	    $(this).parents('.search-form').find('.search-options').slideDown('fast');
+		var menu = $(this).parents('.search-form').find('.search-options');
+		if ( !menu.hasClass('open') ) {
+			menu.slideDown('fast').addClass('open');
+		} else {}
 	    $(document).bind('mousedown', function(e) {
-	        if (! $(e.target).closest('div.search-options').length) {
-	            $("div.search-options").hide();
+	        if (! $(e.target).closest('.search-options, #subnav .contextual-search').length) {
+	            menu.removeClass('open').slideUp('fast', function() {
+		            $('#subnav .expand-menu').show();
+		            $('#subnav .hidden-item').hide();
+	            });
 	            $(document).unbind('mousedown', arguments.callee);
+				$("#subnav .contextual-search").css('width', 215);
 	        }
 	    })
+	});
+
+	//SUBNAV SEARCH EXPAND MENU
+	$('#subnav .expand-menu').click( function() {
+		var totalWidth = 0;
+		$(this).hide();
+		$(this).parents('.nav').find('.hidden-item').show();
+		$(this).parents('.nav').find('li').each(function(index) {
+		    totalWidth += parseInt($(this).width(), 10);
+		});
+		$("#subnav .contextual-search").css('width', totalWidth);
+		return false
 	});
 
 	//RANGE SLIDER

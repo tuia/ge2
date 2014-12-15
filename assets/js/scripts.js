66fbce21ba1efd0c9fb0cef8f8a841ae57a3acaa
Nathália <nathaliazxavier@gmail.com>
2014-12-15 14:53:19 -0200

    [mapa] modal de entidade

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -3,6 +3,23 @@ $(document).ready( function() {
 
 	//BOOTSTRAP
 	$('#poste').modal('show');
+	$('[data-toggle="tooltip"]').tooltip({container: 'body'});
+
+	//SHOW-MORE BTN (...)
+	$('.show-more').click(function() {
+		$(this).parents('.expandable-container').find('.hidden-item').slideToggle();
+	});
+
+	//BOOKMARK BTN
+	$('.bookmark-button').click( function() {
+		if ( $(this).hasClass('marked') ) {
+			$(this).removeClass('marked');
+			$(this).find('.fa').removeClass('fa-bookmark').addClass('fa-bookmark-o');
+		}else {
+			$(this).addClass('marked');
+			$(this).find('.fa').removeClass('fa-bookmark-o').addClass('fa-bookmark');
+		}
+	});
 
 	//SUBNAV SEARCH BOX
 	$("#subnav .contextual-search").bind('focus', function() {
